ccd4420378eecb34aaace1f42042cd70586a77ba
HanquanL <hanquanl0115@gmail.com>
2022-12-29 16:08:42 -0500

    init

---
+++ src/main/java/com/udacity/Data_Multitier_Architecture/datastores/DatastoresApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.Data_Multitier_Architecture.datastores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udacity/Data_Multitier_Architecture/datastores/DatastoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.Data_Multitier_Architecture.datastores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
